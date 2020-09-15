b3b24767c1f4aaf53d2ae65393265fbb806ab8d8
garsalari <71303696+garsalari@users.noreply.github.com>
2020-09-15 18:51:33 -0400

    nuevo servio

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ def name(value):
     val = {"value": value}
     return jsonify(val)
 
+@app.route('/test')
+def test():
+    val = {"return": "1234"}
+    return jsonify(val)
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
